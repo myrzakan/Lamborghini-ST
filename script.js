318a0105a994d35c259fbd0ca7d13cb9e254a0e5
Alan <alanfoureyes@gmail.com>
2023-09-13 14:24:41 +0600

    finish footer

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
